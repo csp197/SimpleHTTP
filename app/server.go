aca4ccbe2b94ea12fa56fbfcb2392dae59716b18
CSP <16367956+CSP197@users.noreply.github.com>
2024-07-17 12:39:33 -0700

    Extracting URL Path

--- app/server.go
+++ app/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	// Uncomment this block to pass the first stage
 	"net"
@@ -15,16 +17,14 @@ func main() {
 
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		log.Printf("Failed to bind to port 4221")
-		// os.Exit(1)
+		log.Fatalln("Failed to bind to port 4221")
 	}
 	// Ensure we close the listener after we're done
 	defer listener.Close()
 
 	connection, err := listener.Accept()
 	if err != nil {
-		log.Printf("Error accepting connection: ", err.Error())
-		// os.Exit(1)
+		log.Fatalln("Error accepting connection: ", err.Error())
 	}
 	// Ensure we close the connection after we're done
 	defer connection.Close()
@@ -32,18 +32,44 @@ func main() {
 	buffer := make([]byte, 1024)
 	numberOfBytes, err := connection.Read(buffer)
 	if err != nil {
-		log.Printf("Error writing to buffer: ", err.Error())
-		// os.Exit(1)
+		log.Fatalln("Error writing to buffer: ", err.Error())
 	}
+	requestPayload := string(buffer[:numberOfBytes])
 	log.Printf("received %d bytes", numberOfBytes)
-	log.Printf("received the following data: %s", string(buffer[:numberOfBytes]))
+	log.Printf("received the following data: %s", requestPayload)
+
+	requestBreakdown := strings.Split(requestPayload, " ")
+
+	// GET                          // HTTP method
+	// /index.html                  // Request target
+	// HTTP/1.1                     // HTTP version
+	// \r\n                         // CRLF that marks the end of the request line
+
+	// // Headers
+	// Host: localhost:4221\r\n     // Header that specifies the server's host and port
+	// User-Agent: curl/7.64.1\r\n  // Header that describes the client's user agent
+	// Accept: */*\r\n              // Header that specifies which media types the client can accept
+	// \r\n                         // CRLF that marks the end of the headers
 
 	responseMessage := []byte("HTTP/1.1 200 OK\r\n\r\n")
+
+	httpMethod := requestBreakdown[0]
+	if httpMethod != "GET" {
+		log.Fatalln(fmt.Sprintf("HTTP method %s is not currently supported", httpMethod))
+	}
+
+	requestTarget := requestBreakdown[1]
+	if requestTarget != "/" {
+		// log.Fatalln(fmt.Sprintf("HTTP target %s is not available", requestTarget))
+		// respond with a 404
+		responseMessage = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+
 	numberOfBytes, err = connection.Write(responseMessage)
 	if err != nil {
-		log.Printf("Error responding: ", err.Error())
-		// os.Exit(1)
+		log.Fatalln("Error responding: ", err.Error())
 	}
 	log.Printf("sent %d bytes", numberOfBytes)
-	log.Printf("sent the following data: %s", string(buffer[:numberOfBytes]))
+	// log.Printf("sent the following data: %s", string(buffer[:numberOfBytes]))
+
 }
