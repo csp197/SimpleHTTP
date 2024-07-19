f25d24a9127b726a917ff5fa71322fc01516c9b9
CSP <16367956+CSP197@users.noreply.github.com>
2024-07-19 10:15:17 -0700

    Read Header

--- app/server.go
+++ app/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"regexp"
 	"strings"
 
 	// Uncomment this block to pass the first stage
@@ -12,17 +11,18 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	log.Printf("Logs from your program will appear here!")
+	log.Printf("Server Running...")
 
 	// Uncomment this block to pass the first stage
 
+	// Creating a TCP listener at port 4221
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		log.Fatalln("Failed to bind to port 4221")
 	}
 	// Ensure we close the listener after we're done
 	defer listener.Close()
-
+	// Accept the incoming connection from the listener at the binded port 4221
 	connection, err := listener.Accept()
 	if err != nil {
 		log.Fatalln("Error accepting connection: ", err.Error())
@@ -30,17 +30,24 @@ func main() {
 	// Ensure we close the connection after we're done
 	defer connection.Close()
 
+	// Initialize byte arr of size 1024 as a buffer to store request/response data
 	buffer := make([]byte, 1024)
+	// Read data from incoming connection into the buffer and return the number of bytes up to which the buffer is written
 	numberOfBytes, err := connection.Read(buffer)
 	if err != nil {
 		log.Fatalln("Error writing to buffer: ", err.Error())
 	}
+	// Typecast the written buffer from byte arr to string to form the request payload
 	requestPayload := string(buffer[:numberOfBytes])
+
+	// Log out the number of bytes written and the request payload
 	log.Printf("received %d bytes", numberOfBytes)
 	log.Printf("received the following data: %s", requestPayload)
 
+	// Tokenize the request payload by space for easy parsing
 	requestBreakdown := strings.Split(requestPayload, " ")
 
+	// Request Breakdown
 	// Status line
 	// GET                          // HTTP method 														// 0
 	// /index.html                  // Request target 													// 1
@@ -53,44 +60,30 @@ func main() {
 	// Accept: */*\r\n              // Header that specifies which media types the client can accept	// 6
 	// \r\n                         // CRLF that marks the end of the headers							// 7
 
+	// 200 Response Message as a byte array
 	responseMessage := []byte("HTTP/1.1 200 OK\r\n")
 
+	// Extract HTTP Method to ensure that it's a GET verb because of limited support of http server
 	httpMethod := requestBreakdown[0]
 	if httpMethod != "GET" {
 		log.Fatalln(fmt.Sprintf("HTTP method %s is not currently supported", httpMethod))
 	}
 
-	regexPattern := "(?m)(/echo)[/a-z0-9]+"
-	regexpStruct, err := regexp.Compile(regexPattern)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// Compile optimized Regex struct using pattern to easily parse the request target
+	// TODO: further improvements here?
+	// regexPattern := "(?m)(/echo)[/a-z0-9]+"
+	// regexpStruct, err := regexp.Compile(regexPattern)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
+	// Extract request target
 	requestTarget := requestBreakdown[1]
 
-	respondWith404 := false
+	// Tokenize the request target by `/` for easy parsing
+	requestTargetBreakdown := strings.Split(requestTarget, "/")
 
-	strInput := ""
-
-	if requestTarget != "/" {
-		regexString := regexpStruct.FindString(requestTarget)
-		if len(regexString) == 0 {
-			respondWith404 = true
-		} else {
-			// TODO extract substring using regex
-			// regexSubstringPattern := "(?m)(/echo)[/a-z0-9]+"
-			// regexpStruct, err := regexp.Compile(regexPattern)
-			strInput = strings.Split(regexString, "/")[2]
-		}
-	}
-	if respondWith404 {
-		responseMessage = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
-	} else if len(strInput) > 0 {
-		responseMessage = []byte(string(responseMessage) + fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(strInput), strInput))
-	} else if len(strInput) == 0 {
-		responseMessage = []byte(string(responseMessage) + "\r\n")
-	}
-
+	// Response Breakdown
 	// Status line
 	// HTTP/1.1 200 OK
 	// \r\n                          // CRLF that marks the end of the status line
@@ -103,10 +96,50 @@ func main() {
 	// Response body
 	// abc                           // The string from the request
 
+	// Check if the request target is an endpoint with no params...
+	// 	  /
+	// [0]/[1]
+	if len(requestTargetBreakdown) == 2 {
+		if requestTarget == "/" { // Check if the request target is the root of the server...
+			// Then add the proper CRLF ending to the pre-existing 200 response
+			responseMessage = []byte(string(responseMessage) + "\r\n")
+		} else if requestTargetBreakdown[1] == "user-agent" { // Check if the request target is the `user-agent` endpoint
+			// Extract client's user agent from request
+			userAgent := requestBreakdown[5]
+			// Redefine the response message with the extracted user agent
+			responseMessage = []byte(
+				string(responseMessage) + fmt.Sprintf(
+					"Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+					len(userAgent),
+					userAgent))
+
+		} else { // If the request target is not supported...
+			// Then redefine the response message as a 404 response
+			responseMessage = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+		}
+	} else { // Otherwise, the request target either has params or has levels...
+		if requestTargetBreakdown[1] == "echo" {
+
+			// Tokenize request target by `/` and access the string input at index 2
+			// /echo/{str}
+			// [0]/[1]/[2]
+			strInput := requestTargetBreakdown[2]
+
+			// Redefine the response message by adding the passed string with headers
+			responseMessage = []byte(
+				string(responseMessage) + fmt.Sprintf(
+					"Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+					len(strInput),
+					strInput))
+		}
+	}
+
+	// Write the response message as a byte array to the connection and get the number of bytes sent out
 	numberOfBytes, err = connection.Write(responseMessage)
 	if err != nil {
 		log.Fatalln("Error responding: ", err.Error())
 	}
+	// Log out the number of bytes and stringify the response message
 	log.Printf("sent %d bytes", numberOfBytes)
 	log.Printf("sent the following data: %s", string(responseMessage))
 
